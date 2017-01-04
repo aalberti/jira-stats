24b5b5fb50d837a1861ffca5cf2ac792f5c6ca41
Antoine Alberti <aalberti@prima-solutions.com>
2017-01-04 12:53:50 +0100

    average lead time

--- src/test/java/aa/TheBigTest.java
+++ src/test/java/aa/TheBigTest.java
@@ -1,5 +1,8 @@
 package aa;
 
+import java.time.Duration;
+import java.util.Optional;
+
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,12 +15,19 @@ public class TheBigTest {
 		try (JiraConnection ignored = jiraConnection) {
 			assertThat(
 				jiraConnection.fetchIssues()
-					.doOnNext(i -> System.out.println("Fetched " + i.getKey()))
+					.doOnNext(i -> System.out.println("Fetched " + i.getKey() + " lead time: " + i.getLeadTime().toString()))
+					.map(Issue::getLeadTime)
+					.filter(Optional::isPresent)
+					.map(Optional::get)
+					.reduce(new long[]{0, 0}, (sumAndCount, leadTime) -> new long[]{sumAndCount[0] + leadTime.toMillis(), sumAndCount[1] + 1} )
+					.map(sumAndCount -> sumAndCount[0]/sumAndCount[1])
+					.map(Duration::ofMillis)
 					.test()
 					.await()
 					.assertComplete()
-					.valueCount()
-			).isBetween(232, 250);
+					.values())
+				.hasSize(1)
+				.allMatch(leadTime -> leadTime.toMillis() < 0);
 		}
 	}
 }
