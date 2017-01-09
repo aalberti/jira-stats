42ecc8a0495cd7f46cf00b5a898f0db9855647c7
Antoine Alberti <aalberti@prima-solutions.com>
2017-01-09 14:45:58 +0100

    Gson serialization

--- src/test/java/aa/TheBigTest.java
+++ src/test/java/aa/TheBigTest.java
@@ -5,9 +5,27 @@ import java.util.Optional;
 
 import org.junit.Test;
 
+import com.google.gson.Gson;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TheBigTest {
+	@Test
+	public void save_the_big_stuff() throws Exception {
+		Gson gson = new Gson();
+		JiraConnection jiraConnection = new JiraConnection();
+		jiraConnection.open();
+		try (JiraConnection ignored = jiraConnection) {
+			assertThat(
+				jiraConnection.fetchIssues()
+					.map(gson::toJson)
+					.doOnNext(j -> System.out.println("Serialized " + j))
+					.test().await()
+					.assertComplete()
+					.values()
+			).isNull();
+		}
+	}
+
 	@Test
 	public void gimme_the_big_stuff() throws Exception {
 		JiraConnection jiraConnection = new JiraConnection();
@@ -19,11 +37,10 @@ public class TheBigTest {
 					.map(Issue::getLeadTime)
 					.filter(Optional::isPresent)
 					.map(Optional::get)
-					.reduce(new long[]{0, 0}, (sumAndCount, leadTime) -> new long[]{sumAndCount[0] + leadTime.toMillis(), sumAndCount[1] + 1} )
+					.reduce(new long[]{0, 0}, (sumAndCount, leadTime) -> new long[]{sumAndCount[0] + leadTime.toMillis(), sumAndCount[1] + 1})
 					.map(sumAndCount -> sumAndCount[0]/sumAndCount[1])
 					.map(Duration::ofMillis)
-					.test()
-					.await()
+					.test().await()
 					.assertComplete()
 					.values())
 				.hasSize(1)
