b99949e634180f92296a53273cd04793b15fab2e
Antoine Alberti <aa@antoinealberti.com>
2017-01-12 10:41:56 +0100

    Remove reactive mongo

--- src/main/java/aa/db/IssueDB.java
+++ src/main/java/aa/db/IssueDB.java
@@ -2,6 +2,7 @@ package aa.db;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.util.function.Consumer;
 
 import org.bson.Document;
 
@@ -9,31 +10,21 @@ import aa.Issue;
 import com.google.gson.Gson;
 import com.mongodb.client.model.UpdateOptions;
 import com.mongodb.client.result.UpdateResult;
-import com.mongodb.reactivestreams.client.MongoClient;
-import com.mongodb.reactivestreams.client.MongoClients;
-import com.mongodb.reactivestreams.client.MongoCollection;
 import io.reactivex.Observable;
-import io.reactivex.subscribers.TestSubscriber;
 import static com.mongodb.client.model.Filters.eq;
 
 public class IssueDB implements Closeable {
-	private MongoClient mongoClient = null;
 	private final Gson gson;
-	private MongoCollection<Document> collection;
-	private com.mongodb.MongoClient syncMongoClient;
-	private com.mongodb.client.MongoCollection<Document> syncCollection;
+	private com.mongodb.MongoClient mongoClient;
+	private com.mongodb.client.MongoCollection<Document> collection;
 
 	public IssueDB() {
 		this.gson = new Gson();
 	}
 
 	public void open() {
-		mongoClient = MongoClients.create();
+		mongoClient = new com.mongodb.MongoClient();
 		collection = mongoClient
-			.getDatabase("jira_stats")
-			.getCollection("issues");
-		syncMongoClient = new com.mongodb.MongoClient();
-		syncCollection = syncMongoClient
 			.getDatabase("jira_stats")
 			.getCollection("issues");
 	}
@@ -43,16 +34,12 @@ public class IssueDB implements Closeable {
 		if (mongoClient != null) {
 			mongoClient.close();
 		}
-		if (syncMongoClient != null) {
-			syncMongoClient.close();
-		}
 		mongoClient = null;
-		syncMongoClient = null;
 	}
 
 	public void save(Issue issue) {
 		SerializedIssue si = new SerializedIssue(issue, gson.toJson(issue));
-		UpdateResult result = syncCollection.replaceOne(
+		UpdateResult result = collection.replaceOne(
 			eq("key", si.issue.getKey()),
 			new Document("key", si.issue.getKey())
 				.append("json", si.json),
@@ -61,22 +48,19 @@ public class IssueDB implements Closeable {
 	}
 
 	public Observable<Issue> readAll() {
-		return Observable.<Document>create(subscriber -> collection.find().subscribe(new TestSubscriber<Document>() {
-			@Override
-			public void onNext(Document document) {
-				subscriber.onNext(document);
+		return Observable.<Document>create(subscriber -> {
+			if (subscriber.isDisposed())
+				return;
+			try {
+				collection.find().forEach((Consumer<? super Document>) subscriber::onNext);
 			}
-
-			@Override
-			public void onError(Throwable t) {
-				subscriber.onError(t);
+			catch (Exception e) {
+				subscriber.onError(e);
 			}
-
-			@Override
-			public void onComplete() {
-				subscriber.onComplete();
-			}
-		})).map(d -> gson.fromJson(d.getString("json"), Issue.class));
+			if (subscriber.isDisposed())
+				return;
+			subscriber.onComplete();
+		}).map(d -> gson.fromJson(d.getString("json"), Issue.class));
 	}
 
 	private class SerializedIssue {
