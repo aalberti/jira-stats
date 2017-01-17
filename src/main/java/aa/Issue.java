3f39d1953eea8e08cf3f2d6d3e086745c64c9e2f
Antoine Alberti <aa@antoinealberti.com>
2017-01-17 19:15:32 +0100

    Add basic issue fields

--- src/main/java/aa/Issue.java
+++ src/main/java/aa/Issue.java
@@ -2,25 +2,77 @@ package aa;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 import static java.util.Comparator.comparing;
 
 public class Issue {
-	private String key;
-	private List<Transition> history;
-	private Instant creationDate;
-	private String project;
+	private final String key;
+	private final String project;
+	private final String status;
+	private final String type;
+	private final String summary;
+	private final String reporter;
+	private final String assignee;
+	private final Collection<String> fixVersions;
+	private final Instant creationDate;
+	private final Instant updateDate;
+	private final List<Transition> history;
 
-	private Issue(String key, String project, Instant creationDate, List<Transition> history) {
+	private Issue(String key, String project, String status, String type, String summary, String reporter, String assignee, Collection<String> fixVersions,
+		Instant creationDate, Instant updateDate, List<Transition> history) {
 		this.key = key;
 		this.project = project;
+		this.status = status;
+		this.type = type;
+		this.summary = summary;
+		this.reporter = reporter;
+		this.assignee = assignee;
+		this.fixVersions = fixVersions;
 		this.creationDate = creationDate;
+		this.updateDate = updateDate;
 		this.history = history;
 	}
 
 	public String getKey() { return key; }
+
+	public String getProject() {
+		return project;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public Collection<String> getFixVersions() {
+		return fixVersions;
+	}
+
+	public String getAssignee() {
+		return assignee;
+	}
+
+	public String getReporter() {
+		return reporter;
+	}
+
+	public String getSummary() {
+		return summary;
+	}
+
+	public Instant getCreationDate() {
+		return creationDate;
+	}
+
+	public Instant getUpdateDate() {
+		return updateDate;
+	}
 
 	public List<Transition> getHistory() { return history; }
 
@@ -38,18 +90,17 @@ public class Issue {
 			.map(Transition::getAt);
 	}
 
-	public Instant getCreationDate() {
-		return creationDate;
-	}
-
-	public String getProject() {
-		return project;
-	}
-
 	public static class Builder {
 		private String key;
 		private String project;
+		private String status;
+		private String type;
+		private String summary;
+		private String reporter;
+		private String assignee;
+		private Collection<String> fixVersions;
 		private Instant creationDate;
+		private Instant updateDate;
 		private List<Transition> history;
 
 		private Builder() {}
@@ -66,8 +117,13 @@ public class Issue {
 			return this;
 		}
 
-		public Builder withHistory(List<Transition> history) {
-			this.history = history;
+		public Builder withStatus(String status) {
+			this.status = status;
+			return this;
+		}
+
+		public Builder withType(String type) {
+			this.type = type;
 			return this;
 		}
 
@@ -76,8 +132,38 @@ public class Issue {
 			return this;
 		}
 
+		public Builder withUpdateDate(Instant updateDate) {
+			this.updateDate = updateDate;
+			return this;
+		}
+
+		public Builder withSummary(String summary) {
+			this.summary = summary;
+			return this;
+		}
+
+		public Builder withReporter(String reporter) {
+			this.reporter = reporter;
+			return this;
+		}
+
+		public Builder withAssignee(String assignee) {
+			this.assignee = assignee;
+			return this;
+		}
+
+		public Builder withFixVersions(Collection<String> fixVersions) {
+			this.fixVersions = fixVersions;
+			return this;
+		}
+
+		public Builder withHistory(List<Transition> history) {
+			this.history = history;
+			return this;
+		}
+
 		public Issue build() {
-			return new Issue(key, project, creationDate, history);
+			return new Issue(key, project, status, type, summary, reporter, assignee, fixVersions, creationDate, updateDate, history);
 		}
 	}
 }
--- src/main/java/aa/jira/IssueMapper.java
+++ src/main/java/aa/jira/IssueMapper.java
@@ -9,10 +9,16 @@ import org.joda.time.DateTime;
 
 import aa.Issue;
 import aa.Transition;
+import com.atlassian.jira.rest.client.api.domain.BasicProject;
 import com.atlassian.jira.rest.client.api.domain.ChangelogGroup;
 import com.atlassian.jira.rest.client.api.domain.ChangelogItem;
+import com.atlassian.jira.rest.client.api.domain.IssueType;
+import com.atlassian.jira.rest.client.api.domain.Status;
+import com.atlassian.jira.rest.client.api.domain.User;
+import com.atlassian.jira.rest.client.api.domain.Version;
 import static aa.Issue.Builder.issue;
 import static aa.Transition.Builder.transition;
+import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toList;
 
 class IssueMapper {
@@ -22,10 +28,33 @@ class IssueMapper {
 			.collect(toList());
 		return issue()
 			.withKey(jiraIssue.getKey())
-			.withProject(jiraIssue.getProject().getKey())
+			.withProject(toKey(jiraIssue.getProject()))
+			.withStatus(toName(jiraIssue.getStatus()))
+			.withType(toName(jiraIssue.getIssueType()))
+			.withSummary(jiraIssue.getSummary())
+			.withReporter(toEmailAddress(jiraIssue.getReporter()))
+			.withAssignee(toEmailAddress(jiraIssue.getAssignee()))
 			.withCreationDate(toInstant(jiraIssue.getCreationDate()))
+			.withUpdateDate(toInstant(jiraIssue.getUpdateDate()))
+			.withFixVersions(stream(jiraIssue.getFixVersions()).map(Version::getName).collect(toList()))
 			.withHistory(history)
 			.build();
+	}
+
+	private String toName(IssueType issueType) {
+		return ofNullable(issueType).map(IssueType::getName).orElse(null);
+	}
+
+	private String toName(Status status) {
+		return ofNullable(status).map(Status::getName).orElse(null);
+	}
+
+	private String toKey(BasicProject project) {
+		return ofNullable(project).map(BasicProject::getKey).orElse(null);
+	}
+
+	private String toEmailAddress(User user) {
+		return ofNullable(user).map(User::getEmailAddress).orElse(null);
 	}
 
 	private Stream<Transition> toHistory(ChangelogGroup changelogGroup) {
@@ -42,10 +71,10 @@ class IssueMapper {
 	}
 
 	private static Instant toInstant(DateTime dateTime) {
-		return Instant.ofEpochMilli(dateTime.getMillis());
+		return ofNullable(dateTime).map(t -> Instant.ofEpochMilli(t.getMillis())).orElse(null);
 	}
 
 	public static <T> Stream<T> stream(Iterable<T> ts) {
-		return StreamSupport.stream(ts.spliterator(), false);
+		return ofNullable(ts).map(i -> StreamSupport.stream(i.spliterator(), false)).orElse(Stream.empty());
 	}
 }
