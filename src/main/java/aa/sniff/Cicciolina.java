36d165fbcbe04df0894c269de88bbe6500b50619
Antoine Alberti <aa@antoinealberti.com>
2017-01-19 12:22:42 +0100

    Cicciolina output format

--- src/main/java/aa/sniff/Cicciolina.java
+++ src/main/java/aa/sniff/Cicciolina.java
@@ -30,16 +30,16 @@ public class Cicciolina {
 		Instant since = db.getNextBatchStart().orElse(Instant.parse("2016-01-01T00:00:00Z"));
 		Instant until = since.plus(1, DAYS);
 		db.startBatch(until);
-		System.out.println("\n|>>> Batch from " + since.toString() + " to " + until.toString() + " Starting at " + now());
+		System.out.println("\nBatch from " + since.toString() + " to " + until.toString() + " Starting at " + now());
 		jira.fetchIssues(updatedSince(since).updatedUntil(until))
 			.doOnNext(
-				i -> System.out.println("--> Fetched " + i.getKey()
+				i -> System.out.println("  - Fetched " + i.getKey()
 					+ " lead time: " + i.getLeadTime().toString()
 					+ " updated " + i.getHistory().stream().map(Transition::getAt).max(naturalOrder()).toString()))
 			.doOnNext(db::save)
 			.test().await();
 		db.batchDone();
-		System.out.println("\n|>>> Batch done at " + now());
+		System.out.println("Batch done at " + now());
 		return until;
 	}
 }
