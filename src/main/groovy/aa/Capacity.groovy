e43ff73823b5b4a57c8fd22859d70412ae6ea2b1
Antoine Alberti <aa@antoinealberti.com>
2017-02-13 17:11:46 +0100

    Issues closed per sprint

--- src/main/groovy/aa/Capacity.groovy
+++ src/main/groovy/aa/Capacity.groovy
@@ -1,18 +1,48 @@
 package aa
 
 import com.gmongo.GMongo
+import com.google.gson.Gson
+
+import java.time.DayOfWeek
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 
 class Capacity {
     static void main(String[] args) {
         def db = new GMongo().getDB('jira_stats')
-        println 'sprints'
-        db.getCollection('sprints').find()
-                .each { println it }
-        println 'issues'
+        def sprints = db.getCollection('sprints').find().toArray()
+        def gson = new Gson()
         db.getCollection('issues').find()
-                .each { println it }
-        println 'globals'
-        db.getCollection('globals').find()
-                .each { println it }
+                .collect { gson.fromJson(it.json as String, Issue) }
+                .findAll { it.project == 'PRIN' }
+                .findAll { issueClosedInASprint(it, sprints) }
+                .groupBy { it.closureDate.map { toMonday(it) }.orElse(null) }
+                .sort()
+                .each { println "${it.key}=[${it.value.collect { it.key }}]" }
+    }
+
+    private static LocalDateTime toMonday(Instant instant) {
+        LocalDateTime.ofInstant(instant, ZoneId.systemDefault())
+                .with(DayOfWeek.MONDAY)
+                .truncatedTo(ChronoUnit.DAYS)
+    }
+
+    private static boolean issueClosedInASprint(Issue issue, sprints) {
+        sprints
+                .findAll { issue.sprints.contains(it.name) }
+                .any { issueClosedInSprint(issue, it) }
+    }
+
+    private static boolean issueClosedInSprint(Issue issue, sprint) {
+        issue.closureDate.map {
+            it.isBefore(parse(sprint.end)) && it.isAfter(parse(sprint.start))
+        }.orElse(false)
+    }
+
+    private static Instant parse(string) {
+        Instant.from(DateTimeFormatter.ISO_OFFSET_DATE_TIME.parse(string))
     }
 }
